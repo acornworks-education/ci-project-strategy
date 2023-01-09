913e08d9f7ac988ff164bfefa5409fdd88161357
JeongSuk Lee <acornworks.kr@gmail.com>
2023-01-09 19:55:56 +1100

    Update index.js

--- actions/sonarqube-project-name/index.js
+++ actions/sonarqube-project-name/index.js
@@ -4,10 +4,8 @@ const github = require('@actions/github');
 console.log(github.context.payload.pull_request.head.ref);
 
 const repoName = github.context.repo.repo.replace('/', '-');
-const branchName = github.context.branchName('/', '-');
+const branchName = github.context.payload.pull_request.head.ref('/', '-');
 const projectName = `${repoName}-${branchName}`;
-
-
 
 console.log('Project name', projectName);
 core.setOutput('sonarqube_project_name', projectName);
